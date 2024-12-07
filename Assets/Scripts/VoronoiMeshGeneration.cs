9d3b24c24e90422c36ab0b84826b6f9a67949b38
pasyanos <pasyanos.c@gmail.com>
2024-12-07 13:16:12 -0500

    added a UI representation of the tile map

--- Assets/Scripts/VoronoiMeshGeneration.cs
+++ Assets/Scripts/VoronoiMeshGeneration.cs
@@ -1,8 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Delaunay.Geo;
 using System.Linq;
+using UnityEngine.Events;
 
 
 public class VoronoiMeshGeneration : MonoBehaviour
@@ -12,11 +12,17 @@ public class VoronoiMeshGeneration : MonoBehaviour
     [SerializeField] private Vector3 meshOrigin = Vector3.zero;
 
     [Header("Generation Settings")]
-    [SerializeField] private Vector2Int rowsByColumns = new Vector2Int(3, 3);
-    [SerializeField] private Vector2 tileSize = new Vector2(1f, 1f);
+    [SerializeField] private Vector2Int _rowsByColumns = new Vector2Int(3, 3);
+    public Vector2Int rowsByColumns { get { return _rowsByColumns; } }
+    [SerializeField] private Vector2 _tileSize = new Vector2(1f, 1f);
+    public Vector2 tileSize { get { return _tileSize; } }
     [SerializeField] private AnimationCurve kernelCurve;
-    [SerializeField] private bool offsetEvenColumns = true;
-    [SerializeField][Range(0f, 1f)] private float randomAmt = 0f;
+    [SerializeField] private bool _offsetEvenColumns = true;
+    public bool offsetEvenColumns { get { return _offsetEvenColumns; } }
+    [SerializeField][Range(0f, 1f)] private float _randomizePointFactor = 0.2f;
+    public float randomizePointFactor { get { return _randomizePointFactor; } }
+
+    [Space(10)]
     [SerializeField] private Color wallColor = Color.gray;
 
     [Header("Terrain Type Settings")]
@@ -28,8 +34,13 @@ public class VoronoiMeshGeneration : MonoBehaviour
 
     // Runtime Vars
     private TerrainType[,] _generatedTileTypes;
+    // expose terrain types for UI
+    // public TerrainType[,] generatedTileTypes {  get { return _generatedTileTypes; } }
+
     private Vector3[,] _tileLocs;
     private Vector2[,] _terrainLocs2D;
+
+    [HideInInspector] public UnityEvent mapUpdatedEvent = new UnityEvent();
 
     /*
      * I am referring to this loosely as a kernel, because I got the idea from filter kernels
@@ -37,18 +48,14 @@ public class VoronoiMeshGeneration : MonoBehaviour
     */
     private float[,] _kernel;
 
-    private List<Vector2> points2D;
-    private List<TerrainType> terrainTypes;
+    private List<Vector2> _points2D;
+    private List<TerrainType> _terrainTypes;
 
     // the 3D point of the lower left corner of the voronoi diagram
     // this is used to convert 2D diagram points to 3D points in worldspace
     private Vector3 lowerLeftCorner;
     private Rect bounds;
     private int numPoints;
-
-    // private List<LineSegment> _voronoiEdges = null;
-    // Not used
-    // private List<LineSegment> _triangulation = null;
     private Delaunay.Voronoi _voronoi;
 
     #region Unity Callbacks
@@ -114,24 +121,39 @@ public class VoronoiMeshGeneration : MonoBehaviour
         GenerateKernel();
 
         InstantiateTiles();
+
+        mapUpdatedEvent.Invoke();
+    }
+    public Color[,] GetTerrainColors()
+    {
+        int rows = rowsByColumns.x;
+        int cols = rowsByColumns.y;
+        Color[,] ret = new Color[rows, cols];
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                ret[i, j] = Setting(_generatedTileTypes[i,j]).GetColor();
+            }
+        }
+
+        return ret;
     }
     #endregion // Public Facing Methods
 
     #region Mesh Generation Functions
     private void Init(Vector2Int gridDimensions, Vector2 tileSize, TerrainType[,] terrainGrid, Vector2[,] posnOffsets)
     {
-        // wasInit = false;
-
-        // genData = data;
         float lenX = (gridDimensions.x + 1) * tileSize.x;
         float lenY = (gridDimensions.y + 1) * tileSize.y;
         bounds = new Rect(0, 0, lenX, lenY);
 
         lowerLeftCorner = new Vector3(meshOrigin.x - lenX * 0.5f, meshOrigin.y, meshOrigin.z - lenY * 0.5f);
 
-        // translate 2D arrays to lists
-        points2D = new List<Vector2>();
-        terrainTypes = new List<TerrainType>();
+        // translate 2D arrays to lists for Voronoi generation
+        _points2D = new List<Vector2>();
+        _terrainTypes = new List<TerrainType>();
 
         Vector2 startingPt = new Vector2(tileSize.x * 0.5f, tileSize.y * 0.5f);
         Vector2 thisOffset;
@@ -142,18 +164,13 @@ public class VoronoiMeshGeneration : MonoBehaviour
             {
                 thisOffset = new Vector2(i * tileSize.x, j * tileSize.y);
 
-                points2D.Add(startingPt + thisOffset + posnOffsets[i, j]);
-                terrainTypes.Add(terrainGrid[i, j]);
+                _points2D.Add(startingPt + thisOffset + posnOffsets[i, j]);
+                _terrainTypes.Add(terrainGrid[i, j]);
             }
         }
 
-        numPoints = points2D.Count;
-
-        _voronoi = new Delaunay.Voronoi(points2D, null, bounds);
-        // _voronoiEdges = _voronoi.VoronoiDiagram();
-
-        // This is not currently needed
-        // _triangulation = _voronoi.DelaunayTriangulation();
+        numPoints = _points2D.Count;
+        _voronoi = new Delaunay.Voronoi(_points2D, null, bounds);
 
         GenerateMeshVoronoi();
     }
@@ -167,20 +184,15 @@ public class VoronoiMeshGeneration : MonoBehaviour
         {
             for (int i = 0; i < numPoints; i++)
             {
-                TerrainType curType = terrainTypes[i];
+                TerrainType curType = _terrainTypes[i];
 
                 // don't instantiate for water
                 if (curType > TerrainType.WATER)
                 {
                     float yOffset = Setting(curType).GetYValue();
                     Color color = Setting(curType).GetColor();
-                    Vector2 curLocation = points2D[i];
+                    Vector2 curLocation = _points2D[i];
 
-                    //List<Vector2> polygonForSite = _voronoi.Region(curLocation);
-
-                    //// voronoi.region returns points in counterclockwise order, need clockwise order for meshes
-                    //// is there a less stupid way to do this?
-                    //polygonForSite.Reverse();
                     List<Vector2> polygonForSite = SortClockwise(_voronoi.Region(curLocation), curLocation);
 
                     TopFaceMeshInformation newInfo = new TopFaceMeshInformation(polygonForSite, yOffset, color,
@@ -200,60 +212,6 @@ public class VoronoiMeshGeneration : MonoBehaviour
 
                         from = to;
                     }
-
-                    // bug with this so just generating all walls for a tile
-                    // find neighbors and form walls where needed
-                    // List<Vector2> neighbors = _voronoi.NeighborSitesForSite(curLocation);
-
-                    //foreach (var neighbor in neighbors)
-                    //{
-                    //    // debugStr += string.Format(" {0} ", neighbor);
-
-                    //    // is there a better way to do this?
-                    //    int neighborIndex = points2D.IndexOf(neighbor);
-                    //    TerrainType neighborTerrainType = terrainTypes[neighborIndex];
-
-                    //    // "taller" tiles will form the walls for their shorter neighbors
-                    //    // if we don't need to make a wall, we can avoid some expensive calculations
-                    //    if (neighborTerrainType < curType)
-                    //    {
-                    //        List<Vector2> neighborPolygon = SortClockwise(_voronoi.Region(neighbor), neighbor);
-                    //        float lowY = Setting(neighborTerrainType).GetYValue();
-                    //        Color lowColor = Setting(neighborTerrainType).GetColor();
-
-                    //        //Vector2 fromIndex = neighborPolygon.Last();
-                    //        //for (int j = 0; j < neighborPolygon.Count; j++)
-                    //        //{
-                    //        //    Vector2 toIndex = neighborPolygon[j];
-
-                    //        //    // line from fromIndex to toIndex
-                    //        //    if (polygonForSite.Contains(fromIndex)) //&& polygonForSite.Contains(toIndex))
-                    //        //    {
-                    //        //        //Debug.LogError("Found a wall!");
-                    //        //        WallMeshInformation newWall = new WallMeshInformation(fromIndex, toIndex, 
-                    //        //            yOffset, lowY, color, lowColor, lowerLeftCorner);
-                    //        //        terrain.AddWallMeshInfo(newWall);
-                    //        //    }
-
-                    //        //    fromIndex = toIndex;
-                    //        //}
-                    //        Vector2 fromIndex = polygonForSite[polygonForSite.Count - 1];
-
-                    //        for (int j = 0; j < polygonForSite.Count; j++)
-                    //        {
-                    //            Vector2 toIndex = polygonForSite[j];
-
-                    //            if (neighborPolygon.Contains(fromIndex) && neighborPolygon.Contains(toIndex))
-                    //            {
-                    //                WallMeshInformation newWall = new WallMeshInformation(fromIndex, toIndex,
-                    //                    yOffset, lowY, color, lowColor, lowerLeftCorner);
-                    //                terrain.AddWallMeshInfo(newWall);
-                    //            }
-
-                    //            fromIndex = toIndex;
-                    //        }
-                    //    }
-                    //}
                 }
             }
         }
@@ -302,15 +260,15 @@ public class VoronoiMeshGeneration : MonoBehaviour
 
     private void InstantiateTiles()
     {
-        int rows = rowsByColumns.x;
-        int columns = rowsByColumns.y;
+        int rows = _rowsByColumns.x;
+        int columns = _rowsByColumns.y;
 
         _tileLocs = new Vector3[rows, columns];
         _generatedTileTypes = new TerrainType[rows, columns];
         _terrainLocs2D = new Vector2[rows, columns];
 
-        float minusXOffset = (float)(rows - 1) / 2f * tileSize.x;
-        float minusZOffset = (float)(columns - 1) / 2f * tileSize.y;
+        float minusXOffset = (float)(rows - 1) / 2f * _tileSize.x;
+        float minusZOffset = (float)(columns - 1) / 2f * _tileSize.y;
 
         Vector3 columnOffset;
 
@@ -323,17 +281,17 @@ public class VoronoiMeshGeneration : MonoBehaviour
         {
             for (int j = 0; j < columns; ++j)
             {
-                columnOffset = offsetEvenColumns ? new Vector3(0, 0, (i % 2) * 0.5f * tileSize.x)
+                columnOffset = _offsetEvenColumns ? new Vector3(0, 0, (i % 2) * 0.5f * _tileSize.x)
                     : Vector3.zero; // else
 
-                _tileLocs[i, j] = new Vector3(-minusXOffset + tileSize.x * i, 0, -minusZOffset + tileSize.y * j) + columnOffset;
+                _tileLocs[i, j] = new Vector3(-minusXOffset + _tileSize.x * i, 0, -minusZOffset + _tileSize.y * j) + columnOffset;
 
                 if (i > 0 && j > 0 && i < rows - 1 && j < columns - 1)
                 {
 
                     // calculate location on 1x1 perlin texture, then sample it
-                    float perlX = (float)((i + xOff) % rowsByColumns.x) / rowsByColumns.x;
-                    float perlY = (float)((j + yOff) % rowsByColumns.y) / rowsByColumns.y;
+                    float perlX = (float)((i + xOff) % _rowsByColumns.x) / _rowsByColumns.x;
+                    float perlY = (float)((j + yOff) % _rowsByColumns.y) / _rowsByColumns.y;
 
                     float perlin = Mathf.PerlinNoise(perlX, perlY);
                     // modulate by kernel, then multiply by 100 to get a percentage
@@ -387,9 +345,9 @@ public class VoronoiMeshGeneration : MonoBehaviour
             }
         }
 
-        var posnOffsets2D = Generate2DOffsets(rowsByColumns, _generatedTileTypes);
+        var posnOffsets2D = Generate2DOffsets(_rowsByColumns, _generatedTileTypes);
 
-        Init(rowsByColumns, tileSize, _generatedTileTypes, posnOffsets2D);
+        Init(_rowsByColumns, _tileSize, _generatedTileTypes, posnOffsets2D);
     }
 
     private Vector2[,] Generate2DOffsets(Vector2Int size, TerrainType[,] types)
@@ -406,12 +364,12 @@ public class VoronoiMeshGeneration : MonoBehaviour
             {
                 var posn = Vector2.zero;
 
-                var columnOffset = offsetEvenColumns ? new Vector2(0, (i % 2) * 0.5f) : Vector2.zero; // else
+                var columnOffset = _offsetEvenColumns ? new Vector2(0, (i % 2) * 0.5f) : Vector2.zero; // else
 
                 // todo: fancy mods to posn here
                 Vector2 rand = new Vector2(UnityEngine.Random.Range(-0.5f, 0.5f), UnityEngine.Random.Range(-0.5f, 0.5f));
 
-                ret[i, j] = posn + randomAmt * rand + columnOffset;
+                ret[i, j] = posn + _randomizePointFactor * rand + columnOffset;
             }
         }
 
@@ -420,18 +378,18 @@ public class VoronoiMeshGeneration : MonoBehaviour
 
     private void GenerateKernel()
     {
-        _kernel = new float[rowsByColumns.x, rowsByColumns.y];
+        _kernel = new float[_rowsByColumns.x, _rowsByColumns.y];
 
         // could move these to variables if we want more control
         float minScale = 0.1f;
         float maxScale = 1.1f;
 
-        float halfX = Mathf.Ceil(rowsByColumns.x / 2.0f);
-        float halfY = Mathf.Ceil(rowsByColumns.y / 2.0f);
+        float halfX = Mathf.Ceil(_rowsByColumns.x / 2.0f);
+        float halfY = Mathf.Ceil(_rowsByColumns.y / 2.0f);
 
-        for (int i = 0; i < rowsByColumns.x; ++i)
+        for (int i = 0; i < _rowsByColumns.x; ++i)
         {
-            for (int j = 0; j < rowsByColumns.y; ++j)
+            for (int j = 0; j < _rowsByColumns.y; ++j)
             {
                 float locX = Mathf.Clamp01(1.0f - Mathf.Abs(halfX - (i + 1)) / halfX);
                 float locY = Mathf.Clamp01(1.0f - Mathf.Abs(halfY - (j + 1)) / halfY);
@@ -449,9 +407,9 @@ public class VoronoiMeshGeneration : MonoBehaviour
     {
         HashSet<TerrainType> ret = new HashSet<TerrainType>();
         bool notTopRow = (i > 0);
-        bool notBottomRow = (i < rowsByColumns.x - 1);
+        bool notBottomRow = (i < _rowsByColumns.x - 1);
         bool notLeftEdgeCol = (j > 0);
-        bool notRightEdgeCol = (j < rowsByColumns.y - 1);
+        bool notRightEdgeCol = (j < _rowsByColumns.y - 1);
 
         if (notTopRow)
         {
--- Assets/Scripts/VoronoiMeshUI.cs
+++ Assets/Scripts/VoronoiMeshUI.cs
@@ -1,10 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VoronoiMeshUI : MonoBehaviour
 {
     [SerializeField] private VoronoiMeshGeneration meshGenerator;
+
+    [Header("Tile Map")]
+    [SerializeField] private CanvasGroup _tileMapParent;
+    [SerializeField] private RectTransform _tileMapRectTransform;
+    [SerializeField] private GridLayoutGroup _tileMapGrid;
+    private Transform _gridTransform;
+    [SerializeField] private Image _tileUIPrefab;
+    [SerializeField] private int _tileHeight = 5;
+
+    private void Start()
+    {
+        meshGenerator.mapUpdatedEvent.AddListener(UpdateUI);
+
+        // do some setup for UI stuff
+        _gridTransform = _tileMapGrid.transform;
+
+        foreach (Transform child in _gridTransform)
+            Destroy(child.gameObject);
+
+        _tileMapGrid.cellSize = new Vector2(_tileHeight, _tileHeight);
+    }
+
+    private void OnDestroy()
+    {
+        meshGenerator.mapUpdatedEvent.RemoveListener(UpdateUI);
+    }
 
     #region Public Facing Methods
     public void OnClickGenerateTerrain()
@@ -12,4 +39,27 @@ public class VoronoiMeshUI : MonoBehaviour
         meshGenerator.StartGeneration();
     }
     #endregion // Public Facing Methods 
+
+    #region private methods and helpers
+    private void UpdateUI()
+    {
+        // clear previous
+        foreach (Transform child in _gridTransform)
+            Destroy(child.gameObject);
+
+        Vector2Int mapDimensions = meshGenerator.rowsByColumns;
+        var colors = meshGenerator.GetTerrainColors();
+
+        _tileMapRectTransform.sizeDelta = mapDimensions*_tileHeight;
+
+        for (int i = 0; i < mapDimensions.x; i++)
+        {
+            for (int j = 0; j < mapDimensions.y; j++)
+            {
+                Image tile = Instantiate(_tileUIPrefab, _gridTransform);
+                tile.color = colors[i,j];
+            }
+        }
+    }
+    #endregion
 }
